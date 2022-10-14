06f54bbaf387852128fb8622982fdebedce77c5d
Austin Holmes <austin.holmes4@gmail.com>
2022-10-14 10:19:48 -0600

    step 3 done

--- about.js
+++ about.js
@@ -1,14 +1,19 @@
 console.log("hello world");
-
+// const cat = document.querySelector("#small-cat")
 
 
 function handleSubmit(evt) {
 	evt.preventDefault();
 	
-	console.log('form submit');
+	alert("The form has been successfully submitted");
 }
 
+function compliment (event) {
+	event.preventDefault()
+	alert("You look well today!")
+}
 
 let form = document.querySelector('#contact');
 
 form.addEventListener('submit', handleSubmit);
+form.addEventListener('mouseover', compliment)
